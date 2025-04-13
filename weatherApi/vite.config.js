70fea7d153374c67cea9e32bd01dc8c59725664a
prakhar <prakharkushwaha001@gmail.com>
2025-04-13 10:50:36 +0530

    updated deploy Script

--- weatherApi/vite.config.js
+++ weatherApi/vite.config.js
@@ -5,7 +5,7 @@ import {ghPages} from 'vite-plugin-gh-pages'
 
 export default defineConfig({
   plugins: [react() , ghPages()],
-  base: '/Weather-Api/weatherApi',
+  base: '/Weather-Api/weatherApi/',
   server :{
     host : true,
     port : 4578,
